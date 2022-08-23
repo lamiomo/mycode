20c859358a66cc114241855ec212f301bc42d5c2
lamiomo <oomokeji@gmail.com>
2022-08-23 13:12:02 +0000

    my first commit

---
+++ advlist/animal-list1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - simple list"""
+
+def main():
+    # create a list called animal
+    animal = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+
+    # display animal
+    print(animal)
+    
+
+main()
+
---
+++ lab_input/iptaker01.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   print() - display data to std out"""
+
+# below is a function containing our code
+def main():
+
+    # pause the program and wait for the user to provide input
+    user_input = input("Please enter an IPv4 IP address:")
+    
+    # display the input back to the user.
+    print("You told me the IPv4 address is: " + user_input)
+    
+# this calls main
+main()
+
+
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   print() - concatenate vs print a series of items"""
+
+def main():
+
+    # collect string input from the user
+    user_input = input("Please enter an IPv4 IP address:")
+    
+    ## the line below creates a single string that is passed to print()
+    # print("You told me the IPv4 address is: " + user_input)
+    
+    ## print() can be given a series of objects separated by a comma
+    print("You told me the IPv4 address is:", user_input)
+
+main()
+
+
